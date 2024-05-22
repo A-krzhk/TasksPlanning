d768baf9584c484fa304ca3e03a23e2b0d20d8ee
Alena <avkoriazhkina@edu.hse.ru>
2024-05-22 13:28:56 +0300

    Добавлены папки для инфраструктуры MVVM, перенесено основное окно в папку с окнами

--- TasksPlanning/App.xaml.cs
+++ TasksPlanning/App.xaml.cs
@@ -4,9 +4,7 @@ using System.Windows;
 
 namespace TasksPlanning
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
     }
