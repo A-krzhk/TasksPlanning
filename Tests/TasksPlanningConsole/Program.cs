3b71a66454eb71e78ae300cb394796b18bc2345c
Alena <avkoriazhkina@edu.hse.ru>
2024-05-22 13:17:11 +0300

    Создан тестовый консольный проект

---
+++ Tests/TasksPlanningConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TasksPlanningConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
